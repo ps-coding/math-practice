6ea074d35abff620600f40157d07ed2a69912b92
ps-coding <95548088+ps-coding@users.noreply.github.com>
2023-08-02 17:56:50 -0400

    Fixed bugs, added two new power ups, and set up for netlify deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,6 +50,10 @@
 	const yearTag = new Date().getFullYear() <= 2023 ? '' : `-${new Date().getFullYear()}`;
 
 	let selectable = false;
+
+	let carryOver = 0;
+
+	let bonusTime = 0;
 </script>
 
 <svelte:head>
@@ -66,25 +70,61 @@
 
 <header class="top">
 	<!-- svelte-ignore a11y-invalid-attribute -->
-	<h1><a class="title" href="javascript:location.reload()">Math Practice</a></h1>
+	<h1 class="outerTitle"><a class="title" href="javascript:location.reload()">Math Practice</a></h1>
 	{#if correct >= 0 && coins > 0}
-		<button
-			class="button coins"
-			disabled={coins < 2}
-			on:click={() => {
-				if (coins >= 2) {
-					coins -= 2;
-					secondsLeft += 3;
-				}
-			}}
-			><span
-				><svg class="coinsvg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
-					><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-						d="M512 80c0 18-14.3 34.6-38.4 48c-29.1 16.1-72.5 27.5-122.3 30.9c-3.7-1.8-7.4-3.5-11.3-5C300.6 137.4 248.2 128 192 128c-8.3 0-16.4 .2-24.5 .6l-1.1-.6C142.3 114.6 128 98 128 80c0-44.2 86-80 192-80S512 35.8 512 80zM160.7 161.1c10.2-.7 20.7-1.1 31.3-1.1c62.2 0 117.4 12.3 152.5 31.4C369.3 204.9 384 221.7 384 240c0 4-.7 7.9-2.1 11.7c-4.6 13.2-17 25.3-35 35.5c0 0 0 0 0 0c-.1 .1-.3 .1-.4 .2l0 0 0 0c-.3 .2-.6 .3-.9 .5c-35 19.4-90.8 32-153.6 32c-59.6 0-112.9-11.3-148.2-29.1c-1.9-.9-3.7-1.9-5.5-2.9C14.3 274.6 0 258 0 240c0-34.8 53.4-64.5 128-75.4c10.5-1.5 21.4-2.7 32.7-3.5zM416 240c0-21.9-10.6-39.9-24.1-53.4c28.3-4.4 54.2-11.4 76.2-20.5c16.3-6.8 31.5-15.2 43.9-25.5V176c0 19.3-16.5 37.1-43.8 50.9c-14.6 7.4-32.4 13.7-52.4 18.5c.1-1.8 .2-3.5 .2-5.3zm-32 96c0 18-14.3 34.6-38.4 48c-1.8 1-3.6 1.9-5.5 2.9C304.9 404.7 251.6 416 192 416c-62.8 0-118.6-12.6-153.6-32C14.3 370.6 0 354 0 336V300.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 342.6 135.8 352 192 352s108.6-9.4 148.1-25.9c7.8-3.2 15.3-6.9 22.4-10.9c6.1-3.4 11.8-7.2 17.2-11.2c1.5-1.1 2.9-2.3 4.3-3.4V304v5.7V336zm32 0V304 278.1c19-4.2 36.5-9.5 52.1-16c16.3-6.8 31.5-15.2 43.9-25.5V272c0 10.5-5 21-14.9 30.9c-16.3 16.3-45 29.7-81.3 38.4c.1-1.7 .2-3.5 .2-5.3zM192 448c56.2 0 108.6-9.4 148.1-25.9c16.3-6.8 31.5-15.2 43.9-25.5V432c0 44.2-86 80-192 80S0 476.2 0 432V396.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 438.6 135.8 448 192 448z"
-					/></svg
-				>Coins: {coins}</span
-			></button
-		>
+		<div>
+			<button
+				class="button coins"
+				disabled={coins < 2}
+				on:click={() => {
+					if (coins >= 2) {
+						coins -= 2;
+						secondsLeft += 3;
+					}
+				}}
+				><span
+					><svg
+						class="coinsvg"
+						xmlns="http://www.w3.org/2000/svg"
+						height="1em"
+						viewBox="0 0 512 512"
+						><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+							d="M512 80c0 18-14.3 34.6-38.4 48c-29.1 16.1-72.5 27.5-122.3 30.9c-3.7-1.8-7.4-3.5-11.3-5C300.6 137.4 248.2 128 192 128c-8.3 0-16.4 .2-24.5 .6l-1.1-.6C142.3 114.6 128 98 128 80c0-44.2 86-80 192-80S512 35.8 512 80zM160.7 161.1c10.2-.7 20.7-1.1 31.3-1.1c62.2 0 117.4 12.3 152.5 31.4C369.3 204.9 384 221.7 384 240c0 4-.7 7.9-2.1 11.7c-4.6 13.2-17 25.3-35 35.5c0 0 0 0 0 0c-.1 .1-.3 .1-.4 .2l0 0 0 0c-.3 .2-.6 .3-.9 .5c-35 19.4-90.8 32-153.6 32c-59.6 0-112.9-11.3-148.2-29.1c-1.9-.9-3.7-1.9-5.5-2.9C14.3 274.6 0 258 0 240c0-34.8 53.4-64.5 128-75.4c10.5-1.5 21.4-2.7 32.7-3.5zM416 240c0-21.9-10.6-39.9-24.1-53.4c28.3-4.4 54.2-11.4 76.2-20.5c16.3-6.8 31.5-15.2 43.9-25.5V176c0 19.3-16.5 37.1-43.8 50.9c-14.6 7.4-32.4 13.7-52.4 18.5c.1-1.8 .2-3.5 .2-5.3zm-32 96c0 18-14.3 34.6-38.4 48c-1.8 1-3.6 1.9-5.5 2.9C304.9 404.7 251.6 416 192 416c-62.8 0-118.6-12.6-153.6-32C14.3 370.6 0 354 0 336V300.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 342.6 135.8 352 192 352s108.6-9.4 148.1-25.9c7.8-3.2 15.3-6.9 22.4-10.9c6.1-3.4 11.8-7.2 17.2-11.2c1.5-1.1 2.9-2.3 4.3-3.4V304v5.7V336zm32 0V304 278.1c19-4.2 36.5-9.5 52.1-16c16.3-6.8 31.5-15.2 43.9-25.5V272c0 10.5-5 21-14.9 30.9c-16.3 16.3-45 29.7-81.3 38.4c.1-1.7 .2-3.5 .2-5.3zM192 448c56.2 0 108.6-9.4 148.1-25.9c16.3-6.8 31.5-15.2 43.9-25.5V432c0 44.2-86 80-192 80S0 476.2 0 432V396.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 438.6 135.8 448 192 448z"
+						/></svg
+					>Coins: {coins}</span
+				></button
+			>
+			{#if coins >= 10}
+				<button
+					class="button bonusTime"
+					on:click={() => {
+						if (coins >= 10) {
+							coins -= 10;
+							bonusTime++;
+						}
+					}}
+					><span
+						>⚡ Bonus Time ({bonusTime != 0 ? bonusTime.toString() + ' sec ' : ''}to {bonusTime + 1}
+						sec)</span
+					></button
+				>
+			{/if}
+			{#if coins >= 30}
+				<button
+					class="button carryOver"
+					on:click={() => {
+						if (coins >= 30) {
+							coins -= 30;
+							carryOver++;
+						}
+					}}
+					><span
+						>⚡ Carry Over Time ({carryOver != 0 ? carryOver.toString() + 'x ' : ''}to {carryOver +
+							1}x)</span
+					></button
+				>
+			{/if}
+		</div>
 	{/if}
 </header>
 <main>
@@ -151,7 +191,7 @@
 						coins++;
 
 						userInput = '';
-						secondsLeft = 10;
+						secondsLeft = 10 + bonusTime + secondsLeft * carryOver;
 
 						if (correct > highScore) {
 							highScore = correct;
@@ -203,6 +243,8 @@
 				correct = 0;
 				secondsLeft = 10;
 				coins = highScore * 2;
+				carryOver = 0;
+				bonusTime = 0;
 				clearInterval(interval);
 				interval = setInterval(() => {
 					secondsLeft--;
@@ -221,7 +263,7 @@
 	{/if}
 </main>
 <footer class="bottom">
-	Copyright &copy; 2023{yearTag} Prasham Shah. All rights reserved.
+	<a href="/about">About</a> · Copyright &copy; 2023{yearTag} Prasham Shah. All rights reserved.
 </footer>
 
 <style>
@@ -243,9 +285,10 @@
 		width: 100%;
 		justify-content: space-around;
 		align-items: center;
+		align-content: center;
 	}
 
-	h1 {
+	.outerTitle {
 		font-size: 1em;
 	}
 
@@ -407,6 +450,44 @@
 		fill: black;
 	}
 
+	.carryOver {
+		background-color: lightskyblue;
+		color: black;
+		width: 30ch;
+	}
+
+	.carryOver:hover {
+		background-color: darkslateblue;
+		color: white;
+	}
+
+	.carryOver:hover span {
+		display: none;
+	}
+
+	.carryOver:hover:before {
+		content: '$ Buy for 30 coins';
+	}
+
+	.bonusTime {
+		background-color: lightskyblue;
+		color: black;
+		width: 30ch;
+	}
+
+	.bonusTime:hover {
+		background-color: darkslateblue;
+		color: white;
+	}
+
+	.bonusTime:hover span {
+		display: none;
+	}
+
+	.bonusTime:hover:before {
+		content: '$ Buy for 10 coins';
+	}
+
 	.start {
 		background-color: lightgreen;
 		color: black;
@@ -443,11 +524,24 @@
 	}
 
 	/* Media Queries */
-
-	@media screen and (max-width: 550px) {
+	@media screen and (max-width: 950px) {
 		.top {
+			justify-content: center;
 			flex-wrap: wrap;
 		}
+
+		.coins {
+			margin-left: 10px;
+		}
+
+		.carryOver {
+			margin-left: 10px;
+		}
+
+		.bonusTime {
+			margin-left: 10px;
+		}
+
 		.title {
 			flex-basis: 100%;
 		}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,70 @@
+<svelte:head>
+	<title>Math Practice - About</title>
+	<meta
+		name="description"
+		content="Learn about all of the features of the website, Math Practice, and learn how to access it on Github."
+	/>
+	<meta
+		name="keywords"
+		content="about information how to use math practice trainer addition sum subtraction difference multiplication product division quotient root roots exponents exponentiation dynamic difficulty coins currency high score"
+	/>
+</svelte:head>
+
+<h1 id="math-practice">About <a href="/">Math Practice</a></h1>
+<h2 id="how-to-use">How to use</h2>
+<h3 id="question-types">Question types</h3>
+<ul>
+	<li>Answer addition, subtraction, multiplication, division, exponent, and root questions</li>
+	<li>The questions get harder the more questions you answer</li>
+	<li>
+		The difficulty changes at different rates: root and exponent questions increase in number
+		logarithmically while the main four operations increase linearly, and even then multiplication
+		and division increase slower than addition and subtraction
+	</li>
+</ul>
+<h3 id="scores-and-coins">Scores and coins</h3>
+<ul>
+	<li>If you don't answer a question in 10 seconds, you move back a level</li>
+	<li>
+		If you are on level 1 and you fail to answer the question in time, you lose the game and can
+		restart
+	</li>
+	<li>
+		Your high score is saved locally, and your starting number of coins for each round is calculated
+		based on your high score
+	</li>
+	<li>
+		You additionally earn a coin for each correct answer and lose a coin for not answering the
+		question in time
+	</li>
+	<li>
+		Coin values:
+		<ul>
+			<li>2 coins: 3 seconds of extra time on the current question</li>
+			<li>10 coins: Increment the amount of extra time granted after each answered question</li>
+			<li>30 coins: Increment the time multiplier granted after each answered question</li>
+		</ul>
+	</li>
+</ul>
+<h2 id="how-to-run-dev">How to run (dev)</h2>
+<p>
+	Feel free to play around with/contribute to this project if you wish, although if you publish a
+	fork, please give credit.
+</p>
+<ul>
+	<li>Make sure you have <a href="https://nodejs.org/en/" target="_blank">node</a> and npm</li>
+	<li>
+		Clone the repo <a href="https://github.com/ps-coding/math-practice" target="_blank">here</a>
+	</li>
+	<li>Run <code>npm install</code> to get the dependencies</li>
+	<li>Execute <code>npm run dev</code> in the terminal</li>
+	<li>Open your browser to the localhost address displayed</li>
+	<li>Enjoy!</li>
+</ul>
+<i
+	>Some stylistic inspiration (mainly just regarding the text field input) was taken from <a
+		href="https://mathtrainer.ai">https://mathtrainer.ai</a
+	>, but no code was copied. The current and future state of the project is and will be solely based
+	on my ideas, and I am the only developer. I created the project because I felt like such a math
+	app turned into a game would be useful and enjoyable.</i
+>
